d64529d7656120577eb291d16b4d65720fd6d816
Delta Student <sadguru.patil04@gmail.com>
2026-05-03 12:52:17 +0530

    change for deployment

--- src/main/java/com/sadguru/wanderlust/security/JwtUtil.java
+++ src/main/java/com/sadguru/wanderlust/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.sadguru.wanderlust.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -9,9 +10,11 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SECRET = "mysupersecretkeymysupersecretkey";
+//    private final String SECRET = "mysupersecretkeymysupersecretkey";
+      @Value("${JWT_SECRET}")
+      private String secret;
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
 
     public String generateToken(String email,String username) {
         return Jwts.builder()
